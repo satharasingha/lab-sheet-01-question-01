71e2dd5ff003955c7ecdb2da5b1ba238a40b74af
Veru Satharasingha <Veru Satharasingha@DESKTOP-BN34K29>
2024-01-28 19:03:48 +0530

    Add project files.

---
+++ question 01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Question01
+{
+    class Question01
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the length");
+            double length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter the width");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+            double area = length * width;
+            Console.WriteLine($"the area of the rectangle is: {area}");
+            Console.ReadLine();
+        }
+    }
+}
